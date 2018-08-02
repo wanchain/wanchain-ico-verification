09336455518a0784c3836e7f557057fd3c3b3c0f
Jeremiah Goyette <jeremiah@wanchain.org>
2018-08-02 11:47:59 -0500

    Move default file path back to .uploads since private makes the app crash

--- imports/utils/filesPath.js
+++ imports/utils/filesPath.js
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 const getFilesPath = function() {
   const filesPath = process.env.FILES_PATH;
   const meteorBase = __meteor_bootstrap__.serverDir.split(/(\\|\/).meteor/)[0];
-  const defaultPath = `${meteorBase}/private/uploads`;
+  const defaultPath = `${meteorBase}/.uploads`;
 
   return filesPath || defaultPath;
 };
