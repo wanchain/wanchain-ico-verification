8926e2c211c33c92a25e096f69efa3aed1552563
Jeremiah Goyette <jeremiah@wanchain.org>
2018-08-03 13:04:29 -0500

    Restrict users publication to only admins

--- imports/api/publications/index.js
+++ imports/api/publications/index.js
@@ -1,11 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { ICOTokens } from '../collections/icotokens';
 import { ToolConfig } from '../collections/toolconfig';
+import { isAdmin } from '../../utils/users';
 
-Meteor.publish('tokens', function(contractAddress) {
-  if (contractAddress) {
-    return ICOTokens.find({ _id: contractAddress });
+Meteor.publish('tokens', function(address) {
+  if (address) {
+    return ICOTokens.find({ _id: address });
   }
+
   return [];
 });
 
@@ -14,5 +16,12 @@ Meteor.publish('toolconf', function() {
 });
 
 Meteor.publish('users', function() {
+  const user = Meteor.users.findOne(this.userId);
+
+  // ensure user is admin
+  if (! isAdmin(user)) {
+    return [];
+  }
+
   return Meteor.users.find({ 'profile.icoUser': true });
 });
