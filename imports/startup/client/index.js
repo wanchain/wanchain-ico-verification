61444351c2a0b79be056ad9fadf8d904d42023a1
Jeremiah Goyette <jeremiah@wanchain.org>
2018-08-06 16:06:27 -0500

    Default to mainnet instead of testnet

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -5,7 +5,7 @@ import { Session } from 'meteor/session';
 import './layouts';
 import './routes';
 
-Session.set('network', 'testnet');
+Session.set('network', 'mainnet');
 
 Meteor.startup(function() {
   console.log('starting up client...', new Date().getTime())
