643f439bf9a18bc67658fd688cc5dbdb2ff22a8d
Jeremiah Goyette <jeremiah@wanchain.org>
2018-08-07 13:52:06 -0500

    Set up specific route for config, and remove variable route

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,29 +1,28 @@
 import { Session } from 'meteor/session';
-
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
   action(params, queryParams) {
-    Session.set('params',queryParams);
+    Session.set('params', queryParams);
     BlazeLayout.render('mainLayout', {
       content: 'contractVerifier',
     });
   }
 });
 
-FlowRouter.route('/p/:template', {
+FlowRouter.route('/config', {
   action(params, queryParams) {
-    Session.set('params',queryParams);
+    Session.set('params', queryParams);
     BlazeLayout.render('mainLayout', {
-      content: params.template,
+      content: 'config',
     });
   }
 });
 
 FlowRouter.route('/verification', {
   action(params, queryParams) {
-    Session.set('params',queryParams);
+    Session.set('params', queryParams);
     BlazeLayout.render('publicLayout', {
       public: 'verify',
     });
